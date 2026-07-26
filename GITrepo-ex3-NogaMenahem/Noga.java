15a5785efe1984257429529caf331f1029c56e22
“nogahmenahem10-tech <nogahmenahem10@gmail.com>
2026-07-26 21:09:52 +0300

    Update Noga output

--- GITrepo-ex3-NogaMenahem/Noga.java
+++ GITrepo-ex3-NogaMenahem/Noga.java
@@ -1,7 +1,8 @@
 public class Noga {
     public static void main(String[] args) {
-        System.out.println("Noga");
-        System.out.println("Noga");
-        System.out.println("Noga");
+        System.out.println("My name is Noga");
+        System.out.println("My name is Noga");
+        System.out.println("My name is Noga");
+        System.out.println("Updated by Jenkins");
     }
 }
